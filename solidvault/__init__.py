f576b27c294adb1a8cb9a6ade60d5fcd5ca90ec8
Constance <79022298+knstnce@users.noreply.github.com>
2023-01-21 13:01:24 -0500

    Basic web app application :)

--- solidvault/__init__.py
+++ solidvault/__init__.py
@@ -1,10 +1,29 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
-def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
 
-    app.add_url_rule('/', 'index')
 
-    return app
+if __name__ == '__main__':
+    app.run()
+
+
+
+# from flask import Flask, send_from_directory
+# import os
+#
+# from flask import (
+#     Blueprint, render_template, current_app, g
+# )
+#
+#
+# def create_app():
+#     app = Flask(__name__, instance_relative_config=True)
+#
+#     app.add_url_rule('/', 'index.html')
+#
+#     return app
