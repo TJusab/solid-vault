6348ce059a57eb5907fa6c611f531d27d5be5c9a
Constance <79022298+knstnce@users.noreply.github.com>
2023-01-21 13:06:33 -0500

    push with the main

--- solidvault/__init__.py
+++ solidvault/__init__.py
@@ -10,20 +10,3 @@ def hello_world():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
-
-
-
-# from flask import Flask, send_from_directory
-# import os
-#
-# from flask import (
-#     Blueprint, render_template, current_app, g
-# )
-#
-#
-# def create_app():
-#     app = Flask(__name__, instance_relative_config=True)
-#
-#     app.add_url_rule('/', 'index.html')
-#
-#     return app
